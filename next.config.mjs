dfc242c7811c71ebafaf69e6999b6397a7481dff
Vegard Dæmring <44493351+Veggissss@users.noreply.github.com>
2024-10-11 00:28:09 +0200

    Added workflow for hosting next.js page/frontend on github pages.

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,30 @@
 import withPWA from 'next-pwa';
 
 const nextConfig = {
-  // Other Next.js configurations can go here if needed
+  reactStrictMode: true, // Enable React strict mode for better error handling
+
+  output: 'export', // For static export
+  trailingSlash: true, // Ensures all routes are exported with a trailing slash
+
+  // Asset directory for the static export
+  images: {
+    unoptimized: true, // Disable Next.js image optimization for static export
+  },
+
+  // Additional configuration for base path if hosted on GitHub Pages
+  basePath: '/scan-shelf',
+  assetPrefix: '/scan-shelf/',
+
+  // Customize Webpack for any additional modifications
+  webpack: (config) => {
+    // Example: Customize or add loaders
+    return config;
+  },
 };
 
 export default withPWA({
   dest: 'public', // Destination for service worker and related files
   disable: process.env.NODE_ENV === 'development', // Disable in development mode
+  register: true, // Automatically registers the service worker
+  skipWaiting: true, // Automatically activates updated service workers
 })(nextConfig);
