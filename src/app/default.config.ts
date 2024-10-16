aad7accd137fb5d31e2765a2e51eb1f1334f824a
Vegard Dæmring <44493351+Veggissss@users.noreply.github.com>
2024-10-16 16:52:24 +0200

    Update default.config.ts

--- src/app/default.config.ts
+++ src/app/default.config.ts
@@ -1,5 +1,5 @@
 export const defaultSettings = {
-    API_HOST: 'http://localhost:3001',
+    API_HOST: 'https://example.com:3001',
     DICTIONARY_LOOKUP: 'https://jisho.org/search/',
     SNIPPET_WIDTH: 140,
     SNIPPET_HEIGHT: 200,
