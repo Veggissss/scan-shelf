0da6e36a0555e766fb07aeca3a0b8f9be4c07967
Vegard Dæmring <vegarddem@gmail.com>
2024-10-18 11:18:23 +0200

    Fix msw service worker path

--- src/app/env.config.ts
+++ src/app/env.config.ts
@@ -1,8 +1,7 @@
-// Github Pages requires a base path of /scan-shelf, while local development does not.
-// BasePath env var is defined in next.config.mjs
-const basePath : string = process.env.BASE_PATH || "";
-
 const isPagesConfig : boolean = process.env.NEXT_PUBLIC_DEPLOY_ENVIRONMENT === "pages";
 const isDevOrTestEnv : boolean = process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test';
+
+// Github Pages requires a base path of /scan-shelf, while local development does not.
+const basePath = isPagesConfig ? '/scan-shelf' : '';
 
 export { basePath, isPagesConfig, isDevOrTestEnv };
